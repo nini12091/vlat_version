d75fef660c07d875f8f2e02be01148524437e9b4
sky <nini12091@naver.com>
2021-07-29 17:37:56 +0900

    version5

--- myvlat/myvlat/urls.py
+++ myvlat/myvlat/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path('', include('quizzes.urls')),
