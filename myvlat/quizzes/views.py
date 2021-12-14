0f3048c132e94466996d8227c6aab17075f09ce6
sky <nini12091@naver.com>
2021-12-14 11:59:02 +0900

    add

--- myvlat/quizzes/views.py
+++ myvlat/quizzes/views.py
@@ -683,9 +683,10 @@ def exportcsv(request):
     return response
 
 data = None
+file_dir = '/Users/sky/Desktop/'
 
 def read_data(table_name):
-    with open(f'{table_name}.csv','r') as csvfile:
+    with open(file_dir + f'{table_name}.csv','r') as csvfile:
         reader = csv.reader(csvfile)
         global data
         data = list(reader)
@@ -694,7 +695,7 @@ def read_data(table_name):
 def footer(table_name, class_name, bulk_list):
     class_name.objects.bulk_create(bulk_list)
 
-    with open(f'{table_name}.csv','w') as csvfile:
+    with open(file_dir + f'{table_name}.csv','w') as csvfile:
         writer = csv.writer(csvfile)
     return
 
